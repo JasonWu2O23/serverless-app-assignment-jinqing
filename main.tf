fefd9d5d20e58bda7c51e74ff25a2aacafaa6798
JasonWu2O23 <jwu0686@gmail.com>
2023-10-30 22:09:43 +0800

    Updated main.tf, comment out line 36

--- main.tf
+++ main.tf
@@ -33,6 +33,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   queue {
     queue_arn     = aws_sqs_queue.queue.arn
     events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
+    # filter_suffix = ".log"
   }
 }
